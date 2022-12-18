c126a2a9ca9dc30aa808e12286e12738cfcf27cf
Minnia <jasminestockholm@gmail.com>
2022-12-18 14:30:15 +0100

    wip UserProfileScreen

---
+++ ModeOfTransportation/app/screens/UserProfileScreen/UserProfileScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const UserProfileScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>User Profile!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default UserProfileScreen;
