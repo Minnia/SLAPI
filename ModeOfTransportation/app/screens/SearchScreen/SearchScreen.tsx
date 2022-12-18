ea70e2bc7c4aa2af7fb945450a24aba7a3644b3c
Minnia <jasminestockholm@gmail.com>
2022-12-18 13:08:05 +0100

    wip SearchScreen

--- ModeOfTransportation/app/screens/SearchScreen/SearchScreen.tsx
+++ ModeOfTransportation/app/screens/SearchScreen/SearchScreen.tsx
@@ -1,23 +1,51 @@
 import React, {useState} from 'react';
-import {Button, TextInput, View} from 'react-native';
+import {Button, View} from 'react-native';
+import Fuse from 'fuse.js';
 import Spacer from 'react-styled-spacer';
+import CommonInput from '../../components/common/CommonInput';
 
 import * as S from './styled';
 
 const SearchScreen: React.FC = () => {
   const [text, onChangeText] = useState('');
+
+  const options = {
+    isCaseSensitive: false,
+    // includeScore: false,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    // fieldNormWeight: 1,
+    keys: [text],
+  };
+
+  const fuzzySearch = () => {
+    const pattern = text;
+    const fuse = new Fuse([text], options);
+    console.log(fuse.search(pattern), 'pattern', pattern);
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <S.StyledSearchContainer>
-        <TextInput
+        <CommonInput
           placeholder="Sök efter tider här..."
           style={{marginLeft: 4, flex: 1}}
-          onChangeText={onChangeText}
           value={text}
+          onChangeText={onChangeText}
         />
       </S.StyledSearchContainer>
       <Spacer h={20} />
-      <Button title="Sök" onPress={() => console.log('test')} />
+      <View>
+        <Button title="Sök" onPress={fuzzySearch} />
+      </View>
     </View>
   );
 };
--- ModeOfTransportation/app/screens/SearchScreen/styled.ts
+++ ModeOfTransportation/app/screens/SearchScreen/styled.ts
@@ -1,9 +1,13 @@
 import {Dimensions} from 'react-native';
-import styled from 'styled-components/native';
+import styled from 'styled-components';
 
+// @ts-ignore
 export const StyledSearchContainer = styled.View<{}>`
   border-width: 2px;
-  border-color: black;
+  border-color: blue;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10;
+  overflow: hidden;
   height: ${Dimensions.get('screen').height / 15}px;
   width: ${Dimensions.get('screen').width - 100}px;
 `;
