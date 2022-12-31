f50669f4d68b64914d7ec1bee995b1faf488374a
Minnia <jasminestockholm@gmail.com>
2022-12-31 03:14:29 +0100

    check for id and fetch use with id

--- ModeOfTransportation/app/screens/SettingsScreen/SettingsScreen.tsx
+++ ModeOfTransportation/app/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
+// @ts-ignore
 
 const SettingsScreen: React.FC = () => {
+  console.log(process.env.SUPER_USER_ID);
+  const [user, setUser] = useState();
+  const getUser = async (id: string) => {
+    const {data} = await axios.get(`http://localhost:3000/user?id=${id}`);
+    setUser(data.firstName);
+  };
+  useEffect(() => {
+    if (process.env.SUPER_USER_ID) {
+      getUser(process.env.SUPER_USER_ID);
+    }
+  }, []);
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
+      <Text>{user}</Text>
     </View>
   );
 };
