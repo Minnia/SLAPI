bdce06f14726e76a43dc6c6244264f4e56de24cd
Minnia <jasminestockholm@gmail.com>
2023-01-30 20:14:03 +0100

    wrap app in store

--- ModeOfTransportation/index.js
+++ ModeOfTransportation/index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+));
